f4bab6fbda6abddde471694d2b440af0b7b35f31
eyoram <esarama22@gmail.com>
2025-08-12 20:34:09 +0700

    fix:badge-list

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,34 +1,73 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import SkillBadgeList from "../List-Badge-Skill";
+import SkillBadgeList from "../List-Badge-Skill/index";
 import Link from "next/link";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    section?.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(() => {
+    AOS.init({
+      // disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  }, []);
   return (
-    <div className="flex my-8 mx-4 md:mx-8 font-poppins flex-col items-center min-h-screen md:min-h-0">
-      <div
-        className="fixed top-0 left-0 w-full h-[30vh] md:h-[40vh] bg-cover bg-center z-[-1]"
-        style={{ backgroundImage: "url('../bg-header.png')" }}
-      />
+    <div className=" flex my-8 mx-4 md:mx-8 font-poppins flex-col items-center min-h-screen md:min-h-0">
+      <Link href="/">
+        <div
+          className="fixed top-0 left-0 w-full h-[30vh] md:h-[40vh] bg-cover bg-center z-[-1] "
+          style={{ backgroundImage: "url('../bg-header.png')" }}
+        />
+      </Link>
 
       <div
         className={`bg-white w-full max-w-4xl py-6 px-6 md:px-14  relative transition-all duration-300
   ${isMenuOpen ? "rounded-3xl" : "rounded-full lg:rounded-full"}`}
       >
         <div className="flex justify-between items-center">
-          <Image src="/logo-porto.png" alt="Logo" width={90} height={90} />
+          <Link href="/">
+            <Image src="/logo-porto.png" alt="Logo" width={90} height={90} />
+          </Link>
 
           <div className="hidden md:flex gap-8 items-center text-slate-blue text-base">
-            <p>Experiences</p>
-            <p>Project</p>
-            <p>Tools</p>
+            <a
+              onClick={() => scrollToSection("features")}
+              className="relative cursor-pointer 
+              after:content-[''] after:absolute after:left-1/2 after:bottom-0 
+              after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-400 after:ease-out
+              hover:after:left-0 hover:after:w-full"
+            >
+              Experiences
+            </a>
+            <a
+              onClick={() => scrollToSection("projects")}
+              className="relative cursor-pointer 
+              after:content-[''] after:absolute after:left-1/2 after:bottom-0 
+              after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-400 after:ease-out
+              hover:after:left-0 hover:after:w-full"
+            >
+              Project
+            </a>
+            <a
+              onClick={() => scrollToSection("tools")}
+              className="relative cursor-pointer 
+              after:content-[''] after:absolute after:left-1/2 after:bottom-0 
+              after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-400 after:ease-out
+              hover:after:left-0 hover:after:w-full"
+            >
+              Tools
+            </a>
           </div>
 
-          <Link 
-            href="/contact"
-          >
+          <Link href="/contact">
             <button className="hover hidden md:block bg-[#3C5867] hover:bg-slate-blue/80 duration-200 transition-all px-5 py-3 md:py-4 text-white rounded-full hover:cursor-pointer">
               Get Started
             </button>
@@ -79,7 +118,7 @@ export default function Navbar() {
         )}
       </div>
 
-      <div className="bg-[#F7FAFC] h-fit w-fit p-1 mt-16 md:mt-20 mb-8 rounded-full">
+      <div data-aos="fade-up"  className="bg-[#F7FAFC] h-fit w-fit p-1 mt-16 md:mt-20 mb-8 rounded-full">
         <div className="bg-white   px-4 py-2 text-slate-blue rounded-full flex flex-row justify-between gap-2 items-center border-2 border-[#FAFCFE]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -113,8 +152,9 @@ export default function Navbar() {
 
       <div className="relative w-full flex justify-center font-onest text-base md:text-lg mb-6">
         <p className="w-4/5 md:w-3/5 text-center text-text-paragraph font-medium">
-          I&apos;m a versatile designer & developer based in the Indonesia, dedicated
-          to crafting innovative and visually appealing digital experiences.
+          I&apos;m a versatile designer & developer based in the Indonesia,
+          dedicated to crafting innovative and visually appealing digital
+          experiences.
         </p>
       </div>
 
