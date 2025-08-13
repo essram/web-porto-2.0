2b6b70575a33e863bd284145ccd83c9299a2c4ee
eyoram <esarama22@gmail.com>
2025-08-13 08:41:07 +0700

    fix: build prod

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import SkillBadgeList from "../List-Badge-Skill/index";
+import SkillBadgeList from "@/components/List-Badge-Skill";
 import Link from "next/link";
 import AOS from "aos";
 import "aos/dist/aos.css";
